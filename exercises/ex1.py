5e329254d6699ae41a28eae9c5425abf5bd476aa
MarcoCecchetto <marcocecchetto33@gmail.com>
2026-07-28 10:57:14 +0200

    First commit

---
+++ exercises/ex1.py
@@ -0,0 +1,13 @@
+# print("Hello World!") The # charcater put the line in comment 
+# print("Hello Again")
+# print("I like typing this.")
+# print("This is fun.")
+# print("Yay! Printing.")
+# print("I'd much reather you 'not'.")
+# print('I "said" do not touch this.') Added the # to print only one line.
+print("just another annoyng line.")
+
+"""Study drills:
+1. Make your script print another line.
+2. Make your script print only one of the lines.
+3. Put a # (octothorpe) character at the beginning of a line. What did it do? Try to find out what this character does."""
